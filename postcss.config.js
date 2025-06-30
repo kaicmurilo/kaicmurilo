f71eacadf159fdc381134bbef2c8404876a5d286
Kaic Nunes <kaicmurilo@live.com>
2025-06-29 22:24:58 -0400

    melhoria componentes

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
+// postcss.config.js
 module.exports = {
   plugins: [
-    require('@tailwindcss/postcss'),
+    require('postcss-flexbugs-fixes'),
+    require('postcss-preset-env')({
+      autoprefixer: { flexbox: 'no-2009' },
+      stage: 3,
+    }),
+    require('tailwindcss'),
     require('autoprefixer'),
   ],
 }
