619b27c9676702b50c12b1efdb9c92016aeada09
Kaic Nunes <kaicmurilo@live.com>
2025-06-29 23:33:36 -0400

    Prepare portfolio for deploy

--- craco.config.js
+++ craco.config.js
@@ -1,17 +1,17 @@
-// craco.config.js
+
 module.exports = {
-  style: {
-    postcss: {
-      mode: 'file'
-    }
-  },
   webpack: {
     configure: (webpackConfig) => {
       const smRule = webpackConfig.module.rules.find(
-        (r) => r.enforce === 'pre' && r.loader && r.loader.includes('source-map-loader')
+        (r) =>
+          r.enforce === 'pre' &&
+          r.loader &&
+          r.loader.includes('source-map-loader')
       );
-      if (smRule) smRule.exclude = /@mediapipe\/tasks-vision/;
+      if (smRule) {
+        smRule.exclude = /@mediapipe\/tasks-vision/;
+      }
       return webpackConfig;
-    }
-  }
+    },
+  },
 };
